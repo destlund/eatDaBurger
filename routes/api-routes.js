2a1c2402a548b176f6c84751304254a7bdf526cd
David Estlund <destlund@gmail.com>
2018-02-14 17:36:09 -0600

    This should be working but it isn't. API doesn't recognize 'text'

---
+++ routes/api-routes.js
@@ -0,0 +1,77 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+
+// Requiring our models
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // GET route for getting all of the burgers
+  app.get("/api/burgers", function(req, res) {
+    // findAll returns all entries for a table when used with no options
+    db.Burger.findAll({}).then(function(dbburger) {
+      // We have access to the burgers as an argument inside of the callback function
+      res.json(dbburger);
+    });
+  });
+
+  // POST route for saving a new burger
+  app.post("/api/burgers", function(req, res) {
+    // create takes an argument of an object describing the item we want to
+    // insert into our table. In this case we just we pass in an object with a text
+    // and complete property (req.body)
+    db.Burger.create({
+      "text": req.body.text
+    }).then(function(dbburger) {
+      // We have access to the new burger as an argument inside of the callback function
+      res.json(dbburger);
+    })
+    .catch(function(err) {
+      // Whenever a validation or flag fails, an error is thrown
+      // We can "catch" the error to prevent it from being "thrown", which could crash our node app
+      res.json(err);
+    });
+  });
+
+  // DELETE route for deleting burgers. We can get the id of the burger to be deleted from
+  // req.params.id
+  app.delete("/api/burgers/:id", function(req, res) {
+    // We just have to specify which burger we want to destroy with "where"
+    db.Burger.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbburger) {
+      res.json(dbburger);
+    });
+
+  });
+
+  // PUT route for updating burgers. We can get the updated burger data from req.body
+  app.put("/api/burgers", function(req, res) {
+
+    // Update takes in an object describing the properties we want to update, and
+    // we use where to describe which objects we want to update
+    db.Burger.update({
+      text: req.body.text,
+      eaten: req.body.eaten
+    }, {
+      where: {
+        id: req.body.id
+      }
+    }).then(function(dbburger) {
+      res.json(dbburger);
+    })
+    .catch(function(err) {
+      // Whenever a validation or flag fails, an error is thrown
+      // We can "catch" the error to prevent it from being "thrown", which could crash our node app
+      res.json(err);
+    });
+  });
+};
---
+++ server.js
@@ -0,0 +1,52 @@
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Requiring our models for syncing
+var db = require("./models");
+
+// Sets up the Express app to handle data parsing
+
+// // parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }));
+// // parse application/json
+// app.use(bodyParser.json());
+
+// // Static directory
+// app.use(express.static("public"));
+
+// Routes
+// =============================================================
+require("./routes/api-routes.js")(app);
+
+// Routes
+
+app.get("/", function(req, res) {
+  res.render("all-burgers", {
+    foods: db.Burger.findAll({}),
+    eater: "Everyone"
+  });
+});
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
